f84c682f218edcebc71c5da4df2e99b0b783a2bb
MahmoudFarouq <mahmoud.farouk987@gmail.com>
2023-12-04 14:13:23 +0400

    cargo fmt

--- src/day02.rs
+++ src/day02.rs
@@ -4,71 +4,71 @@ use std::collections::HashMap;
 
 #[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
 pub enum Cube {
-	Red,
-	Green,
-	Blue,
+    Red,
+    Green,
+    Blue,
 }
 
 impl From<String> for Cube {
-	fn from(value: String) -> Self {
-		match value.as_str() {
-			"red" => Red,
-			"green" => Green,
-			"blue" => Blue,
-			_ => unreachable!(),
-		}
-	}
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "red" => Red,
+            "green" => Green,
+            "blue" => Blue,
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Set {
-	groups: Vec<(Cube, usize)>,
+    groups: Vec<(Cube, usize)>,
 }
 
 impl Set {
-	fn count(&self, cube: &Cube) -> usize {
-		self.groups
-			.iter()
-			.filter_map(|(c, n)| if c == cube { Some(n) } else { None })
-			.sum()
-	}
+    fn count(&self, cube: &Cube) -> usize {
+        self.groups
+            .iter()
+            .filter_map(|(c, n)| if c == cube { Some(n) } else { None })
+            .sum()
+    }
 }
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Game {
-	id: usize,
-	sets: Vec<Set>,
+    id: usize,
+    sets: Vec<Set>,
 }
 
 impl Game {
-	fn is_possible(&self, values: &[(Cube, usize)]) -> bool {
-		for (c, v) in values {
-			for s in self.sets.iter() {
-				if s.count(c) > *v {
-					return false;
-				}
-			}
-		}
+    fn is_possible(&self, values: &[(Cube, usize)]) -> bool {
+        for (c, v) in values {
+            for s in self.sets.iter() {
+                if s.count(c) > *v {
+                    return false;
+                }
+            }
+        }
 
-		true
-	}
+        true
+    }
 
-	fn least_possible(&self) -> Set {
-		let mut map = HashMap::new();
-		for s in self.sets.iter() {
-			for (g, s) in s.groups.iter() {
-				let e = map.entry(*g).or_insert(0);
+    fn least_possible(&self) -> Set {
+        let mut map = HashMap::new();
+        for s in self.sets.iter() {
+            for (g, s) in s.groups.iter() {
+                let e = map.entry(*g).or_insert(0);
 
-				if *e < *s {
-					*e = *s
-				}
-			}
-		}
+                if *e < *s {
+                    *e = *s
+                }
+            }
+        }
 
-		Set {
-			groups: map.into_iter().collect(),
-		}
-	}
+        Set {
+            groups: map.into_iter().collect(),
+        }
+    }
 }
 
 peg::parser! {
@@ -94,121 +94,121 @@ peg::parser! {
 
 #[aoc_generator(day2)]
 fn parse_day2(input: &str) -> Vec<Game> {
-	input
-		.lines()
-		.map(game_parser::game)
-		.filter_map(Result::ok)
-		.collect()
+    input
+        .lines()
+        .map(game_parser::game)
+        .filter_map(Result::ok)
+        .collect()
 }
 
 #[aoc(day2, part1)]
 fn day2_part1(games: &[Game]) -> usize {
-	const VALUES: [(Cube, usize); 3] = [(Red, 12), (Green, 13), (Blue, 14)];
+    const VALUES: [(Cube, usize); 3] = [(Red, 12), (Green, 13), (Blue, 14)];
 
-	games
-		.iter()
-		.filter_map(|g| {
-			if g.is_possible(&VALUES) {
-				Some(g.id)
-			} else {
-				None
-			}
-		})
-		.sum()
+    games
+        .iter()
+        .filter_map(|g| {
+            if g.is_possible(&VALUES) {
+                Some(g.id)
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 #[aoc(day2, part2)]
 fn day2_part2(games: &[Game]) -> usize {
-	games
-		.iter()
-		.map(Game::least_possible)
-		.map(|s| s.groups.iter().map(|(_, n)| n).product::<usize>())
-		.sum::<usize>()
+    games
+        .iter()
+        .map(Game::least_possible)
+        .map(|s| s.groups.iter().map(|(_, n)| n).product::<usize>())
+        .sum::<usize>()
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_parse_number() {
-		let input = "123";
+    #[test]
+    fn test_parse_number() {
+        let input = "123";
 
-		assert_eq!(game_parser::number(input), Ok(123))
-	}
+        assert_eq!(game_parser::number(input), Ok(123))
+    }
 
-	#[test]
-	fn test_parse_identifier() {
-		let input = "green";
+    #[test]
+    fn test_parse_identifier() {
+        let input = "green";
 
-		assert_eq!(game_parser::identifier(input), Ok("green".to_string()))
-	}
+        assert_eq!(game_parser::identifier(input), Ok("green".to_string()))
+    }
 
-	#[test]
-	fn test_parse_group() {
-		let input = "14 green";
+    #[test]
+    fn test_parse_group() {
+        let input = "14 green";
 
-		assert_eq!(game_parser::group(input), Ok((Green, 14)))
-	}
+        assert_eq!(game_parser::group(input), Ok((Green, 14)))
+    }
 
-	#[test]
-	fn test_parse_set() {
-		let input = "3 blue, 4 red";
+    #[test]
+    fn test_parse_set() {
+        let input = "3 blue, 4 red";
 
-		assert_eq!(
-			game_parser::set(input),
-			Ok(Set {
-				groups: vec![(Blue, 3), (Red, 4)]
-			})
-		)
-	}
+        assert_eq!(
+            game_parser::set(input),
+            Ok(Set {
+                groups: vec![(Blue, 3), (Red, 4)]
+            })
+        )
+    }
 
-	#[test]
-	fn test_parse_game() {
-		let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+    #[test]
+    fn test_parse_game() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
 
-		assert_eq!(
-			game_parser::game(input),
-			Ok(Game {
-				id: 1,
-				sets: vec![
-					Set {
-						groups: vec![(Blue, 3), (Red, 4)]
-					},
-					Set {
-						groups: vec![(Red, 1), (Green, 2), (Blue, 6)]
-					},
-					Set {
-						groups: vec![(Green, 2)]
-					},
-				],
-			})
-		)
-	}
+        assert_eq!(
+            game_parser::game(input),
+            Ok(Game {
+                id: 1,
+                sets: vec![
+                    Set {
+                        groups: vec![(Blue, 3), (Red, 4)]
+                    },
+                    Set {
+                        groups: vec![(Red, 1), (Green, 2), (Blue, 6)]
+                    },
+                    Set {
+                        groups: vec![(Green, 2)]
+                    },
+                ],
+            })
+        )
+    }
 
-	#[test]
-	fn test_part1() {
-		let input = [
-			"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-			"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-			"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-			"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-			"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-		]
-			.join("\n");
-		assert_eq!(day2_part1(&parse_day2(&input)), 8);
-	}
+    #[test]
+    fn test_part1() {
+        let input = [
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ]
+        .join("\n");
+        assert_eq!(day2_part1(&parse_day2(&input)), 8);
+    }
 
-	#[test]
-	fn test_part2() {
-		let input = [
-			"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-			"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-			"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-			"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-			"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-		]
-			.join("\n");
-		assert_eq!(day2_part2(&parse_day2(&input)), 2286);
-	}
+    #[test]
+    fn test_part2() {
+        let input = [
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        ]
+        .join("\n");
+        assert_eq!(day2_part2(&parse_day2(&input)), 2286);
+    }
 }
--- src/day04.rs
+++ src/day04.rs
@@ -1,22 +1,23 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use itertools::Itertools;
+use itertools::{min, Itertools};
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Card {
-	id: usize,
-	winning: Vec<usize>,
-	have: Vec<usize>,
+    id: usize,
+    winning: Vec<usize>,
+    have: Vec<usize>,
 }
 
 impl Card {
-	/// count returns the count of numbers we have that are winning numbers.
-	fn count(&self) -> usize {
-		*self.have
-			.iter()
-			.counts_by(|n| self.winning.contains(n))
-			.get(&true)
-			.unwrap_or(&0)
-	}
+    /// count returns the count of numbers we have that are winning numbers.
+    fn count(&self) -> usize {
+        *self
+            .have
+            .iter()
+            .counts_by(|n| self.winning.contains(n))
+            .get(&true)
+            .unwrap_or(&0)
+    }
 }
 
 peg::parser! {
@@ -24,106 +25,116 @@ peg::parser! {
     pub rule number() -> usize
       = n:$(['0'..='9']+) {? n.parse().or(Err("usize")) }
 
-	pub rule sep() -> String = n:$([' ']+) { n.to_string() }
+    pub rule sep() -> String = n:$([' ']+) { n.to_string() }
 
     pub rule set() -> Vec<usize> = numbers:(number() ** sep())
 
     pub rule card() -> Card
       = "Card" " "* id:(number()) ":" " "*  winning:(set()) " "* "|" " "*  have:(set()) { Card{id, winning, have} }
 
-	pub rule cards() -> Vec<Card> = cards:(card() ** "\n")
+    pub rule cards() -> Vec<Card> = cards:(card() ** "\n")
   }
 }
 
 #[aoc_generator(day4)]
 fn parse_day4(input: &str) -> Vec<Card> {
-	card_parser::cards(input).unwrap()
+    card_parser::cards(input).unwrap()
 }
 
 #[aoc(day4, part1)]
 fn day4_part1(cards: &[Card]) -> usize {
-	cards.iter().map(Card::count).map(|s| if s > 0 {
-		2_usize.pow((s - 1) as _)
-	} else {
-		0
-	}).sum()
+    cards
+        .iter()
+        .map(Card::count)
+        .map(|s| if s > 0 { 2_usize.pow((s - 1) as _) } else { 0 })
+        .sum()
 }
-
 
 #[aoc(day4, part2)]
 fn day4_part2(cards: &[Card]) -> usize {
-	struct G {
-		count: usize,
-		score: usize,
-	}
+    struct G {
+        count: usize,
+        score: usize,
+    }
 
-	let mut scores: Vec<G> = cards.iter().map(|c| G { count: 1, score: c.count() }).collect();
+    let mut scores: Vec<G> = cards
+        .iter()
+        .map(|c| G {
+            count: 1,
+            score: c.count(),
+        })
+        .collect();
 
-	for i in 0..scores.len() {
-		let upper_bound = if i + scores[i].score > scores.len() { scores.len() } else { i + scores[i].score };
+    for i in 0..scores.len() {
+        let upper_bound = *min(&[i + scores[i].score, scores.len()]).unwrap();
+        for j in i + 1..=upper_bound {
+            scores[j].count += scores[i].count;
+        }
+    }
 
-		for j in i + 1..=upper_bound {
-			scores[j].count += scores[i].count;
-		}
-	}
-
-	scores.iter().map(|r| r.count).sum()
+    scores.iter().map(|r| r.count).sum()
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_parse_number() {
-		let input = "123";
+    #[test]
+    fn test_parse_number() {
+        let input = "123";
 
-		assert_eq!(card_parser::number(input), Ok(123))
-	}
+        assert_eq!(card_parser::number(input), Ok(123))
+    }
 
-	#[test]
-	fn test_parse_set() {
-		let input = "83 86  6 31 17  9 48 53";
+    #[test]
+    fn test_parse_set() {
+        let input = "83 86  6 31 17  9 48 53";
 
-		assert_eq!(card_parser::set(input), Ok(vec![83, 86, 6, 31, 17, 9, 48, 53]))
-	}
+        assert_eq!(
+            card_parser::set(input),
+            Ok(vec![83, 86, 6, 31, 17, 9, 48, 53])
+        )
+    }
 
-	#[test]
-	fn test_parse_card() {
-		let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+    #[test]
+    fn test_parse_card() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
 
-		assert_eq!(card_parser::card(input), Ok(Card {
-			id: 1,
-			winning: vec![41, 48, 83, 86, 17],
-			have: vec![83, 86, 6, 31, 17, 9, 48, 53],
-		}))
-	}
+        assert_eq!(
+            card_parser::card(input),
+            Ok(Card {
+                id: 1,
+                winning: vec![41, 48, 83, 86, 17],
+                have: vec![83, 86, 6, 31, 17, 9, 48, 53],
+            })
+        )
+    }
 
-	#[test]
-	fn test_part1() {
-		let input = [
-			"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-			"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-			"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-			"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-			"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-			"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
-		]
-			.join("\n");
-		assert_eq!(day4_part1(&parse_day4(&input)), 13);
-	}
+    #[test]
+    fn test_part1() {
+        let input = [
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        ]
+        .join("\n");
+        assert_eq!(day4_part1(&parse_day4(&input)), 13);
+    }
 
-	#[test]
-	fn test_part2() {
-		let input = [
-			"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-			"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-			"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-			"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-			"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-			"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
-		]
-			.join("\n");
-		assert_eq!(day4_part2(&parse_day4(&input)), 30);
-	}
+    #[test]
+    fn test_part2() {
+        let input = [
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        ]
+        .join("\n");
+        assert_eq!(day4_part2(&parse_day4(&input)), 30);
+    }
 }
